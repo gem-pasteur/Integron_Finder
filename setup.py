5f8fdc9a43064c47aed0b493d2a53000293d81ed
Bertrand Néron <bneron@pasteur.fr>
2016-10-28 14:32:31 +0200

    fix expand_data: which generate extra path

--- setup.py
+++ setup.py
@@ -128,13 +128,25 @@ def subst_vars(src, dst, vars):
 
 
 def expand_data(data_to_expand):
+    """
+    From data structure like setup.py data_files (see http://)
+     [(directory/where/to/copy/the/file, [path/to/file/in/archive/file1, ...]), ...]
+    but instead of the original struct this one accept to specify a directory in elements to copy.
+
+    This function will generate one entry for all *content* of the directory and subdirectory
+    recursively, to in fine copy the tree in archive in dest on the host
+
+    the first level of directory itself is not include (which allow to rename it)
+    :param data_to_expand:
+    :type  data_to_expand: list of tuple
+    :return: list of tuple
+    """
     def remove_prefix(prefix, path):
         prefix = os.path.normpath(prefix)
         path = os.path.normpath(path)
-        to_keep = os.path.split(prefix)[1]
-        truncated = path[len(to_keep):]
-        if truncated.startswith(os.path.sep):
-            truncated = truncated[1:]
+        to_remove = len([i for i in prefix.split(os.path.sep) if i])
+        truncated = [i for i in path.split(os.path.sep) if i][to_remove:]
+        truncated = os.path.sep.join(truncated)
         return truncated
 
     data_struct = []
@@ -146,7 +158,8 @@ def expand_data(data_to_expand):
                     if not files:
                         continue
                     path_2_create = remove_prefix(one_src, path)
-                    data_struct.append((os.path.join(base_dest_dir, path_2_create), [os.path.join(path, f) for f in files]))
+                    data_struct.append(
+                        (os.path.join(base_dest_dir, path_2_create), [os.path.join(path, f) for f in files]))
             if os.path.isfile(one_src):
                 data_struct.append((base_dest_dir, [one_src]))
     return data_struct
