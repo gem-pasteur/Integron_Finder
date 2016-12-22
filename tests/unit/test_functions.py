025103b2e8f2e8bbab4f8ce77bdacfdef6dbae34
Amandine PERRIN <amandine.perrin@pasteur.fr>
2016-12-22 11:59:00 +0100

    Add start of tests for search_attc
    For now, 2 tests:
    - when there are only attC sites clustered and on same strand (nothing to change)
    - when there are some attC sites on strand - and another on strand + (but all clustered)

---
+++ tests/unit/test_functions.py
@@ -0,0 +1,108 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+
+"""
+Unit tests for functions in integron_finder.py
+"""
+
+import integron_finder
+import pandas as pd
+import unittest
+import os
+import pandas.util.testing as pdt
+
+
+class TestFunctions(unittest.TestCase):
+
+    def test_search_attc_uniq(self):
+        """
+        Test that it finds a unique attc array when giving a table with 3 attC sites
+        on the same strand and separated by less than 4kb each.
+        """
+        rep_name = "acba.007.p01.13"
+        attc_file = os.path.join("tests", "data", "Results_Integron_Finder_" + rep_name,
+                                 "other", rep_name + "_attc_table.res")
+        integron_finder.replicon_name = rep_name  # name of replicon in given fasta file
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+        # Construct attC dataframe (read from infernal file)
+        attc_df = integron_finder.read_infernal(attc_file)
+        # search attC arrays, keeping palindromes
+        # 2 attc sites are in the same array if they are on the same strand, and separated by
+        # a distance less than 4kb
+        integron_finder.DISTANCE_THRESHOLD = 4000  # (4kb at least between 2 different arrays)
+        integron_finder.SIZE_REPLICON = 20301  # size of acba.007.p01.13
+        attc_array = integron_finder.search_attc(attc_df, True)
+        self.assertEqual(len(attc_array), 1)
+
+        # Construct expected output:
+        attc_res = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut", "cm_fin", "pos_beg", "pos_end", "sens", "evalue"], dtype='int')
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
+                                    "pos_end": 17884, "sens": "-", "evalue": 1e-9},
+                                    ignore_index=True)
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
+                                    "pos_end": 19149, "sens": "-", "evalue": 1e-4},
+                                    ignore_index=True)
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 19618,
+                                    "pos_end": 19726, "sens": "-", "evalue": 1.1e-7},
+                                    ignore_index=True)
+        # convert positions to int
+        intcols = ["cm_debut", "cm_fin", "pos_beg", "pos_end"]
+        attc_res[intcols] = attc_res[intcols].astype(int)
+        pdt.assert_frame_equal(attc_res, attc_array[0])
+
+    def test_search_attc_diff_strand(self):
+        """
+        Test that it finds a 2 attc arrays when giving a table with 3 attC sites
+        on the same strand and 1 on the other strand, all separated by less than 4kb each.
+        """
+        rep_name = "acba.007.p01.13"
+        attc_file = os.path.join("tests", "data", "Results_Integron_Finder_" + rep_name,
+                                 "other", rep_name + "_attc_table.res")
+        integron_finder.replicon_name = rep_name  # name of replicon in given fasta file
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+        # Construct attC dataframe (read from infernal file)
+        attc_df = integron_finder.read_infernal(attc_file)
+        # Add another attC on the opposite strand
+        attc_df = attc_df.append({"Accession_number": rep_name, "cm_attC": "attC_4", "cm_debut": 1,
+                                  "cm_fin": 47, "pos_beg": 15000, "pos_end": 16000, "sens": "+",
+                                  "evalue": 0.19}, ignore_index=True)
+        # search attC arrays, keeping palindromes
+        # 2 attc sites are in the same array if they are on the same strand, and separated by
+        # a distance less than 4kb
+        integron_finder.DISTANCE_THRESHOLD = 4000  # (4kb at least between 2 different arrays)
+        integron_finder.SIZE_REPLICON = 20301  # size of acba.007.p01.13
+        attc_array = integron_finder.search_attc(attc_df, True)
+        self.assertEqual(len(attc_array), 2)
+
+        # Construct expected outputs:
+        attc_res = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut", "cm_fin",
+                                         "pos_beg", "pos_end", "sens", "evalue"])
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
+                                    "pos_end": 17884, "sens": "-", "evalue": 1e-9},
+                                    ignore_index=True)
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
+                                    "pos_end": 19149, "sens": "-", "evalue": 1e-4},
+                                    ignore_index=True)
+        attc_res = attc_res.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                    "cm_debut": 1, "cm_fin": 47, "pos_beg": 19618,
+                                    "pos_end": 19726, "sens": "-", "evalue": 1.1e-7},
+                                    ignore_index=True)
+        attc_res2 = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut", "cm_fin",
+                                          "pos_beg", "pos_end", "sens", "evalue"])
+        attc_res2 = attc_res2.append({"Accession_number": rep_name, "cm_attC": "attC_4",
+                                      "cm_debut": 1, "cm_fin": 47, "pos_beg": 15000,
+                                      "pos_end": 16000, "sens": "+", "evalue": 0.19},
+                                     ignore_index=True)
+        # convert positions to int
+        intcols = ["cm_debut", "cm_fin", "pos_beg", "pos_end"]
+        attc_res[intcols] = attc_res[intcols].astype(int)
+        attc_res2[intcols] = attc_res2[intcols].astype(int)
+        pdt.assert_frame_equal(attc_res, attc_array[1])
+        pdt.assert_frame_equal(attc_res2, attc_array[0])
+
