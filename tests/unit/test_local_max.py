3f06440948a3f74950cbb3b312747f66fc8291de
Bertrand Néron <bneron@pasteur.fr>
2017-06-23 14:07:42 +0200

    unit test for local_max function

--- tests/unit/test_local_max.py
+++ tests/unit/test_local_max.py
@@ -15,6 +15,7 @@ warnings.simplefilter('ignore', BiopythonExperimentalWarning)
 
 import integron_finder
 _call_ori = integron_finder.call
+_read_infernal_ori = integron_finder.read_infernal
 
 from tests import which
 
@@ -38,23 +39,30 @@ def call_wrapper():
     return wrapper
 
 
-def read_infernal_mock():
+def read_infernal_mock(tmp_dir):
     """expand call several time local_max which run cmserach and need lot of global variable
     to avoid to call cmsearch (we don't want to test local max), I have been ccahed the results
     of local_max and replace the function by this mock
     """
-    _cache = {('/tmp/tmp_test_integron_finder/lian.001.c02.10_942899_947099_subseq_attc_table.res', 1.0, 200, 40):
-               pd.DataFrame([
-                   ['lian.001.c02.10', 'attC_4', 1, 47, 371, 496, '+', 0.130000],
-                   ['lian.001.c02.10', 'attC_4', 1, 47, 1109, 1234, '+', 0.049000]
-                   ['lian.001.c02.10', 'attC_4', 1, 47, 1573, 1699, '+', 0.000005]],
-                   columns=['df_max', 'Accession_number', 'cm_attC', 'cm_debut',
-                            'cm_fin', 'pos_beg', 'pos_end', 'sens', 'evalue']),
-
+    _cache = {
+        (os.path.join(tmp_dir, 'lian.001.c02.10_942899_947099_subseq_attc_table.res'), 1.0, 200, 40):
+            pd.DataFrame([
+                ['lian.001.c02.10', 'attC_4', 1, 47, 371, 496, '+', 0.130000],
+                ['lian.001.c02.10', 'attC_4', 1, 47, 1109, 1234, '+', 0.049000],
+                ['lian.001.c02.10', 'attC_4', 1, 47, 1573, 1699, '+', 0.000005]],
+                columns=['Accession_number', 'cm_attC', 'cm_debut',
+                         'cm_fin', 'pos_beg', 'pos_end', 'sens', 'evalue']),
+        (os.path.join(tmp_dir, 'lian.001.c02.10_946899_951099_subseq_attc_table.res'), 1.0, 200, 40):
+            pd.DataFrame(columns=['Accession_number', 'cm_attC', 'cm_debut',
+                         'cm_fin', 'pos_beg', 'pos_end', 'sens', 'evalue']),
+        (os.path.join(tmp_dir, 'lian.001.c02.10_930689_934889_subseq_attc_table.res'), 1.0, 200, 40):
+            pd.DataFrame(columns=['Accession_number', 'cm_attC', 'cm_debut',
+                         'cm_fin', 'pos_beg', 'pos_end', 'sens', 'evalue']),
               }
-    def fake_local_max(*args):
+    def fake_read_infernal(tblout_path, evalue=None, size_max_attc=None, size_min_attc=None):
+        args = (tblout_path, evalue, size_max_attc, size_min_attc)
         return _cache[args]
-    return fake_local_max
+    return fake_read_infernal
 
 
 class TestLocalMax(unittest.TestCase):
@@ -83,9 +91,11 @@ class TestLocalMax(unittest.TestCase):
         integron_finder.MODEL_attc = os.path.join(self.integron_home, 'data', 'Models', 'attc_4.cm')
         integron_finder.out_dir = self.tmp_dir
         integron_finder.call = call_wrapper()
+        integron_finder.read_infernal = read_infernal_mock(self.tmp_dir)
 
     def tearDown(self):
         integron_finder.call = _call_ori
+        integron_finder.read_infernal = _read_infernal_ori
         try:
             shutil.rmtree(self.tmp_dir)
             pass
@@ -93,7 +103,7 @@ class TestLocalMax(unittest.TestCase):
             pass
 
 
-    def test_local_max(self):
+    def test_local_max_top(self):
         integron_finder.replicon_name = 'lian.001.c02.10'
         replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
 
@@ -117,3 +127,133 @@ class TestLocalMax(unittest.TestCase):
                                           columns=['Accession_number', 'cm_attC', 'cm_debut', 'cm_fin', 'pos_beg',
                                                    'pos_end', 'sens', 'evalue'])
         pdt.assert_frame_equal(local_max_expected, local_max_recieved)
+
+
+    def test_no_local_max_top(self):
+        integron_finder.replicon_name = 'lian.001.c02.10'
+        replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
+
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+
+        win_beg = 946899
+        win_end = 951099
+        strand_search = 'top'
+        local_max_recieved = integron_finder.local_max(integron_finder.replicon_name,
+                                                       win_beg, win_end,
+                                                       strand_search=strand_search)
+        local_max_expected = pd.DataFrame(columns=['Accession_number', 'cm_attC', 'cm_debut', 'cm_fin', 'pos_beg',
+                                                   'pos_end', 'sens', 'evalue'])
+        pdt.assert_frame_equal(local_max_expected, local_max_recieved)
+
+
+    def test_no_local_max_bottom(self):
+        integron_finder.replicon_name = 'lian.001.c02.10'
+        replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
+
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+
+        win_beg = 930689
+        win_end = 934889
+        strand_search = 'bottom'
+        local_max_recieved = integron_finder.local_max(integron_finder.replicon_name,
+                                                       win_beg, win_end,
+                                                       strand_search=strand_search)
+        local_max_expected = pd.DataFrame(columns=['Accession_number', 'cm_attC', 'cm_debut', 'cm_fin', 'pos_beg',
+                                                   'pos_end', 'sens', 'evalue'])
+        pdt.assert_frame_equal(local_max_expected, local_max_recieved)
+
+
+    def test_local_max_both(self):
+        integron_finder.replicon_name = 'lian.001.c02.10'
+        replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
+
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+
+        win_beg = 942899
+        win_end = 947099
+        strand_search = 'both'
+        local_max_recieved = integron_finder.local_max(integron_finder.replicon_name,
+                                                       win_beg, win_end,
+                                                       strand_search=strand_search)
+        local_max_expected = pd.DataFrame([['lian.001.c02.10', 'attC_4', 1, 47, 943270, 943395, '+', 0.13],
+                                           ['lian.001.c02.10', 'attC_4', 1, 47, 944008, 944133, '+', 0.049],
+                                           ['lian.001.c02.10', 'attC_4', 1, 47, 944472, 944598, '+', 4.7e-06]],
+                                          columns=['Accession_number', 'cm_attC', 'cm_debut', 'cm_fin', 'pos_beg',
+                                                   'pos_end', 'sens', 'evalue'])
+        pdt.assert_frame_equal(local_max_expected, local_max_recieved)
+
+
+    def local_max_end_beg(self):
+        integron_finder.replicon_name = 'lian.001.c02.10'
+        replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
+
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+
+        win_end = 942899
+        win_beg = 947099
+        strand_search = 'top'
+        local_max_recieved = integron_finder.local_max(integron_finder.replicon_name,
+                                                       win_beg, win_end,
+                                                       strand_search=strand_search)
+        local_max_expected = pd.DataFrame([['lian.001.c02.10', 'attC_4', 1, 47, 943270, 943395, '+', 0.13],
+                                           ['lian.001.c02.10', 'attC_4', 1, 47, 944008, 944133, '+', 0.049],
+                                           ['lian.001.c02.10', 'attC_4', 1, 47, 944472, 944598, '+', 4.7e-06]],
+                                          columns=['Accession_number', 'cm_attC', 'cm_debut', 'cm_fin', 'pos_beg',
+                                                   'pos_end', 'sens', 'evalue'])
+        pdt.assert_frame_equal(local_max_expected, local_max_recieved)
+
+    def test_local_max_cmsearch_failed(self):
+        integron_finder.replicon_name = 'lian.001.c02.10'
+        replicon_path = os.path.join(self._data_dir, integron_finder.replicon_name + '.fst')
+
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+
+        win_beg = 942899
+        win_end = 947099
+        strand_search = 'top'
+
+        integron_finder.CMSEARCH = 'failed_cmsearch'
+        with self.assertRaises(RuntimeError) as ctx:
+            _ = integron_finder.local_max(integron_finder.replicon_name,
+                                          win_beg, win_end,
+                                          strand_search=strand_search)
+        self.assertEqual(ctx.exception.message,
+                         "{} failed : [Errno 2] No such file or directory".format(integron_finder.CMSEARCH))
+
+        integron_finder.call = lambda x: 1
+        with self.assertRaises(RuntimeError) as ctx:
+            _ = integron_finder.local_max(integron_finder.replicon_name,
+                                          win_beg, win_end,
+                                          strand_search=strand_search)
+        self.assertEqual(ctx.exception.message, "{} failed returncode = {}".format(integron_finder.CMSEARCH,
+                                                                                   integron_finder.call(None)))
