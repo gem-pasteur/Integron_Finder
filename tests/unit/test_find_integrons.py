4f3812a04b55568d262081e1f2f04b91eb3e7422
Bertrand Néron <bneron@pasteur.fr>
2017-06-26 14:15:08 +0200

    add test wit --no_proteins == False
    also remove dependence to csv file, create dataFrame results on the fly

--- tests/unit/test_find_integrons.py
+++ tests/unit/test_find_integrons.py
@@ -10,6 +10,8 @@ from contextlib import contextmanager
 import pandas as pd
 import pandas.util.testing as pdt
 
+import numpy as np
+
 # display warning only for non installed integron_finder
 from Bio import BiopythonExperimentalWarning
 from Bio import Seq, SeqIO
@@ -112,37 +114,27 @@ class TestFindIntegons(unittest.TestCase):
         integron = integrons[0]
         self.assertEqual(integron.ID_replicon, replicon_name)
 
-        exp = pd.read_csv(os.path.join(self._data_dir,'{}_integrase.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
+        exp = pd.DataFrame({'annotation': ['attC'] * 3,
+                            'distance_2attC': [np.nan, 1196.0, 469.0],
+                            'evalue': [1.000000e-09, 1.000000e-04, 1.100000e-07],
+                            'model': ['attc_4'] * 3,
+                            'pos_beg': [17825, 19080, 19618],
+                            'pos_end': [17884, 19149, 19726],
+                            'strand': [-1, -1, -1],
+                            'type_elt': 'attC'}, index=['attc_001', 'attc_002', 'attc_003'])
+        pdt.assert_frame_equal(integron.attC, exp)
+
+        exp = pd.DataFrame(columns=["pos_beg", "pos_end", "strand",
+                                    "evalue", "type_elt", "model",
+                                    "distance_2attC", "annotation"])
+
+        exp = exp.astype(dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
+                                "evalue": "float", "type_elt": "str", "model": "str",
+                                "distance_2attC": "float", "annotation": "str"})
 
         pdt.assert_frame_equal(integron.integrase, exp)
-        exp = pd.read_csv(os.path.join(self._data_dir,'{}_attC.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
-        pdt.assert_frame_equal(integron.attC, exp)
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_promoter.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
         pdt.assert_frame_equal(integron.promoter, exp)
-
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_attI.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
         pdt.assert_frame_equal(integron.attI, exp)
-
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_proteins.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
         pdt.assert_frame_equal(integron.proteins, exp)
 
 
@@ -192,35 +184,108 @@ class TestFindIntegons(unittest.TestCase):
         integron = integrons[0]
         self.assertEqual(integron.ID_replicon, replicon_name)
 
-        exp = pd.read_csv(os.path.join(self._data_dir,'{}_integrase.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
+        exp = pd.DataFrame({'annotation': ['attC'] * 3,
+                            'distance_2attC': [np.nan, 1196.0, 469.0],
+                            'evalue': [1.000000e-09, 1.000000e-04, 1.100000e-07],
+                            'model': ['attc_4'] * 3,
+                            'pos_beg': [17825, 19080, 19618],
+                            'pos_end': [17884, 19149, 19726],
+                            'strand': [-1, -1, -1],
+                            'type_elt': 'attC'}, index=['attc_001', 'attc_002', 'attc_003'])
+        pdt.assert_frame_equal(integron.attC, exp)
+
+        exp = pd.DataFrame(columns=["pos_beg", "pos_end", "strand",
+                                    "evalue", "type_elt", "model",
+                                    "distance_2attC", "annotation"])
+
+        exp = exp.astype(dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
+                                "evalue": "float", "type_elt": "str", "model": "str",
+                                "distance_2attC": "float", "annotation": "str"})
 
         pdt.assert_frame_equal(integron.integrase, exp)
-        exp = pd.read_csv(os.path.join(self._data_dir,'{}_attC.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
+        pdt.assert_frame_equal(integron.promoter, exp)
+        pdt.assert_frame_equal(integron.attI, exp)
+        pdt.assert_frame_equal(integron.proteins, exp)
 
+
+    def test_find_integron_proteins(self):
+        replicon_name = 'acba.007.p01.13'
+        replicon_path = os.path.join(self._data_dir, replicon_name + '.fst')
+        attc_file = os.path.join(self._data_dir,
+                                 'Results_Integron_Finder_acba.007.p01.13/other/acba.007.p01.13_attc_table.res')
+        intI_file = os.path.join(self._data_dir,
+                                 'Results_Integron_Finder_acba.007.p01.13/other/acba.007.p01.13_intI.res')
+        phageI_file = os.path.join(self._data_dir,
+                                   'Results_Integron_Finder_acba.007.p01.13/other/acba.007.p01.13_phage_int.res')
+        args = argparse.Namespace
+        args.no_proteins = False
+        args.keep_palindromes = True
+        args.union_integrases = False
+        args.gembase = False  # needed by read_hmm which is called when no_proteins == False
+
+        integron_finder.replicon_name = replicon_name
+        integron_finder.SEQUENCE = SeqIO.read(replicon_path, "fasta", alphabet=Seq.IUPAC.unambiguous_dna)
+        integron_finder.SIZE_REPLICON = len(integron_finder.SEQUENCE)
+        integron_finder.args = args
+        integron_finder.evalue_attc = 1.
+        integron_finder.max_attc_size = 200
+        integron_finder.min_attc_size = 40
+        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+        integron_finder.DISTANCE_THRESHOLD = 4000  # (4kb at least between 2 different arrays)
+        integron_finder.model_attc_name = integron_finder.MODEL_attc.split("/")[-1].split(".cm")[0]
+
+
+        with self.catch_output() as (out, err):
+            integrons = integron_finder.find_integron(replicon_name,
+                                                      attc_file,
+                                                      intI_file,
+                                                      phageI_file)
+
+        self.assertEqual(err.getvalue().strip(), "")
+        self.assertEqual(out.getvalue().strip(), """In replicon acba.007.p01.13, there are:
+- 1 complete integron(s) found with a total 3 attC site(s)
+- 0 CALIN element(s) found with a total of 0 attC site(s)
+- 0 In0 element(s) found with a total of 0 attC site""")
+
+        self.assertEqual(len(integrons), 1)
+        integron = integrons[0]
+        self.assertEqual(integron.ID_replicon, replicon_name)
+
+        exp = pd.DataFrame({'annotation': 'intI',
+                            'distance_2attC': np.nan,
+                            'evalue':  1.900000e-25,
+                            'model': 'intersection_tyr_intI',
+                            'pos_beg': 55,
+                            'pos_end': 1014,
+                            'strand': 1,
+                            'type_elt': 'protein'}, index=['ACBA.007.P01_13_1'])
+        exp = exp.astype(dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
+                                "evalue": "float", "type_elt": "str", "model": "str",
+                                "distance_2attC": "float", "annotation": "str"})
+        pdt.assert_frame_equal(integron.integrase, exp)
+
+        exp = pd.DataFrame({'annotation': ['attC'] * 3,
+                            'distance_2attC': [np.nan, 1196.0,  469.0],
+                            'evalue':  [1.000000e-09, 1.000000e-04, 1.100000e-07],
+                            'model': ['attc_4'] * 3,
+                            'pos_beg': [17825, 19080, 19618],
+                            'pos_end': [17884, 19149, 19726],
+                            'strand': [-1, -1, -1],
+                            'type_elt': 'attC'}, index=['attc_001', 'attc_002', 'attc_003'])
+
+        exp = exp.astype(dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
+                                "evalue": "float", "type_elt": "str", "model": "str",
+                                "distance_2attC": "float", "annotation": "str"})
         pdt.assert_frame_equal(integron.attC, exp)
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_promoter.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
+
+        exp = pd.DataFrame(columns=["pos_beg", "pos_end", "strand",
+                                    "evalue", "type_elt", "model",
+                                    "distance_2attC", "annotation"])
+
+        exp = exp.astype(dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
+                                "evalue": "float", "type_elt": "str", "model": "str",
+                                "distance_2attC": "float", "annotation": "str"})
 
         pdt.assert_frame_equal(integron.promoter, exp)
-
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_attI.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
         pdt.assert_frame_equal(integron.attI, exp)
-
-        exp = pd.read_csv(os.path.join(self._data_dir, '{}_proteins.csv'.format(replicon_name)),
-                          dtype={"pos_beg": "int", "pos_end": "int", "strand": "int",
-                                 "evalue": "float", "type_elt": "str", "model": "str",
-                                 "distance_2attC": "float", "annotation": "str"})
-
         pdt.assert_frame_equal(integron.proteins, exp)
