6a92466c3079966826f4fd6d63ace818f1880d93
Bertrand Néron <bneron@pasteur.fr>
2017-06-27 15:33:31 +0200

    rewrite test for add_integrase and add_attc

--- tests/unit/test_integron.py
+++ tests/unit/test_integron.py
@@ -26,100 +26,93 @@ class Test(unittest.TestCase):
         # PSSU.001.C01_13_1919, 2131128, 2132135,    1,   5.100000e-27, protein, intI,    intersection_tyr_intI, NaN
 
         id_replicon = "acba.007.p01.13"
-        data_integrase = {"pos_beg" : 55,
-                          "pos_end" : 1014,
-                          "strand" : 1,
-                          "evalue" : 1.900000e-25,
-                          "type_elt" : "protein",
-                          "annotation" : "intI",
-                          "model" : "intersection_tyr_intI",
-                          "distance_2attC" : np.nan}
+        data_integrase = {"pos_beg": 55,
+                          "pos_end": 1014,
+                          "strand": 1,
+                          "evalue": 1.900000e-25,
+                          "type_elt": "protein",
+                          "annotation": "intI",
+                          "model": "intersection_tyr_intI",
+                          "distance_2attC": np.nan}
+        integrase_dtype = {"pos_beg": 'int',
+                           "pos_end": 'int',
+                           "strand": 'int',
+                           "evalue": 'float',
+                           "type_elt": 'str',
+                           "annotation": 'str',
+                           "model": 'str',
+                           "distance_2attC": 'float'}
         id_int = "ACBA.007.P01_13_1"
-        df = pd.DataFrame(columns=["pos_beg", "pos_end", "strand", "evalue", "type_elt",
-                                   "model", "distance_2attC", "annotation"])
-        tmp_df = pd.DataFrame()
-        tmp_df["pos_beg"] = [data_integrase["pos_beg"]]
-        tmp_df["pos_end"] = [data_integrase["pos_end"]]
-        tmp_df["strand"] = [data_integrase["strand"]]
-        tmp_df["evalue"] = [data_integrase["evalue"]]
-        tmp_df["type_elt"] = data_integrase["type_elt"]
-        tmp_df["annotation"] = data_integrase["annotation"]
-        tmp_df["model"] = [data_integrase["model"]]
-        tmp_df.index = [id_int]
-        tmp_df["distance_2attC"] = [np.nan]
-        df = df.append(tmp_df)
+
+        df = pd.DataFrame(data_integrase, index=[id_int])
+        df = df.astype(dtype=integrase_dtype)
+
         integron = Integron(id_replicon)
         integron.add_integrase(data_integrase["pos_beg"],
                                data_integrase["pos_end"],
-                               [id_int],
+                               id_int,
                                data_integrase["strand"],
                                data_integrase["evalue"],
                                data_integrase["model"]
                                )
-        pdt.assert_frame_equal(df, integron.integrase, check_dtype=False)
+        pdt.assert_frame_equal(df, integron.integrase)
+        with self.assertRaises(RuntimeError) as ctx:
+            integron.add_integrase(data_integrase["pos_beg"],
+                                   data_integrase["pos_end"],
+                                   id_int,
+                                   data_integrase["strand"],
+                                   data_integrase["evalue"],
+                                   data_integrase["model"]
+                                   )
+        self.assertEqual(str(ctx.exception), "add_integrase should be called once.")
+
 
     def test_add_attc(self):
         id_replicon = "acba.007.p01.13"
-        data_attc = {"pos_beg" : 10,
-                          "pos_end" : 100,
-                          "strand" : -1,
-                          "evalue" : 1.1e-07,
-                          "type_elt" : "attC",
-                          "annotation" : "attC",
-                          "model" : "attc_4"}
+        data_attc_1 = {"pos_beg": 10,
+                       "pos_end": 100,
+                       "strand": -1,
+                       "evalue": 1.1e-07,
+                       "type_elt": "attC",
+                       "annotation": "attC",
+                       "model": "attc_4",
+                       "distance_2attC": np.nan}
+        attc_dtype = {"pos_beg": 'int',
+                      "pos_end": 'int',
+                      "strand": 'int',
+                      "evalue": 'float',
+                      "type_elt": 'str',
+                      "annotation": 'str',
+                      "model": 'str',
+                      "distance_2attC": 'float'}
 
-        df = pd.DataFrame(columns=["pos_beg", "pos_end", "strand", "evalue", "type_elt",
-                                   "model", "distance_2attC", "annotation"])
-        tmp_df = pd.DataFrame()
-        tmp_df["pos_beg"] = [data_attc["pos_beg"]]
-        tmp_df["pos_end"] = [data_attc["pos_end"]]
-        tmp_df["strand"] = [data_attc["strand"]]
-        tmp_df["evalue"] = [data_attc["evalue"]]
-        tmp_df["type_elt"] = "attC"
-        tmp_df["annotation"] = "attC"
-        tmp_df["model"] = [data_attc["model"]]
-
-        df = df.append(tmp_df, ignore_index=True)
-        sizes_cassettes = [np.nan]
-        df["distance_2attC"] = sizes_cassettes
-        df.index = ["attc_%03i" % int(j + 1) for j in df.index]
-
+        attc_1 = pd.DataFrame(data_attc_1, index=['attc_001'])
+        attc_1 = attc_1.astype(dtype=attc_dtype)
         integron = Integron(id_replicon)
-        integron.add_attC(data_attc["pos_beg"],
-                          data_attc["pos_end"],
-                          data_attc["strand"],
-                          data_attc["evalue"],
-                          data_attc["model"])
+        integron.add_attC(attc_1.loc['attc_001', 'pos_beg'],
+                          attc_1.loc['attc_001', 'pos_end'],
+                          attc_1.loc['attc_001', 'strand'],
+                          attc_1.loc['attc_001', 'evalue'],
+                          attc_1.loc['attc_001', 'model'])
 
-        pdt.assert_frame_equal(df, integron.attC, check_dtype=False)
-        self.assertListEqual(sizes_cassettes, integron.sizes_cassettes)
+        pdt.assert_frame_equal(attc_1, integron.attC)
 
         integron_finder.SIZE_REPLICON = 3
 
-        tmp_df2 = pd.DataFrame()
-        tmp_df2["pos_beg"] = [data_attc["pos_beg"] + 100]
-        tmp_df2["pos_end"] = [data_attc["pos_end"] + 100]
-        tmp_df2["strand"] = [data_attc["strand"]]
-        tmp_df2["evalue"] = [data_attc["evalue"]]
-        tmp_df2["type_elt"] = "attC"
-        tmp_df2["annotation"] = "attC"
-        tmp_df2["model"] = [data_attc["model"]]
-        sizes_cassettes.append((
-                                (data_attc["pos_beg"] + 100) -
-                                (data_attc["pos_end"])) %
-                                integron_finder.SIZE_REPLICON)
+        attc_2 = pd.DataFrame(data_attc_1, index=['attc_002'])
+        attc_2['pos_beg'] = attc_2['pos_beg'] + 100
+        attc_2['pos_end'] = attc_2['pos_end'] + 100
+        attc_2["distance_2attC"] = (attc_2.loc['attc_002', 'pos_beg'] - attc_1.loc['attc_001', 'pos_end']) % \
+                                   integron_finder.SIZE_REPLICON
+        attc = attc_1.append(attc_2)
 
-        df = df.append(tmp_df2, ignore_index=True)
-        df["distance_2attC"] = sizes_cassettes
-        df.index = ["attc_%03i" % int(j + 1) for j in df.index]
-        integron.add_attC(data_attc["pos_beg"] + 100,
-                          data_attc["pos_end"] + 100,
-                          data_attc["strand"],
-                          data_attc["evalue"],
-                          data_attc["model"])
+        integron.add_attC(attc_2.loc['attc_002', 'pos_beg'],
+                          attc_2.loc['attc_002', 'pos_end'],
+                          attc_2.loc['attc_002', 'strand'],
+                          attc_2.loc['attc_002', 'evalue'],
+                          attc_2.loc['attc_002', 'model'])
+        pdt.assert_frame_equal(attc, integron.attC)
 
-        pdt.assert_frame_equal(df, integron.attC, check_dtype=False)
-        self.assertListEqual(sizes_cassettes, integron.sizes_cassettes)
 
     def test_type(self):
         no_integrase = Integron("foo")
