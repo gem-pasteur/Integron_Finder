3dd2a3d63ba0dc371f52b4c6768a9a9440aeeb49
Bertrand Néron <bneron@pasteur.fr>
2018-02-17 18:04:42 +0100

    :art: refactor read_infernal
    put read_infernal in infernal module
    remove global variable
    fix pep8
    fix unit test according to new api

--- integron_finder/infernal.py
+++ integron_finder/infernal.py
@@ -1,7 +1,11 @@
+import os
+from subprocess import call
 import pandas as pd
+from Bio import SeqIO
 
 
-def read_infernal(infile, evalue=1, size_max_attc=200, size_min_attc=40):
+def read_infernal(infile, replicon_name, len_model_attc,
+                  evalue=1, size_max_attc=200, size_min_attc=40):
     """
     Function that parse cmsearch --tblout output and returns a pandas DataFrame
     """
@@ -12,13 +16,12 @@ def read_infernal(infile, evalue=1, size_max_attc=200, size_min_attc=40):
         return pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                      "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
 
-    df = pd.read_table(infile, sep="\s*", engine="python",
-                       header=None, skipfooter=10, skiprows=2)
+    df = pd.read_table(infile, sep="\s*", engine="python",  header=None, skipfooter=10, skiprows=2)
     # Keep only columns: query_name(2), mdl from(5), mdl to(6), seq from(7),
     # seq to(8), strand(9), E-value(15)
     df = df[[2, 5, 6, 7, 8, 9, 15]]
     df = df[(df[15] < evalue)]  # filter on evalue
-    df = df[(abs(df[8]-df[7]) < size_max_attc) & (size_min_attc < abs(df[8]-df[7]))]
+    df = df[(abs(df[8] - df[7]) < size_max_attc) & (size_min_attc < abs(df[8] - df[7]))]
     if len(df) > 0:
         df["Accession_number"] = replicon_name
         c = df.columns.tolist()
@@ -29,10 +32,10 @@ def read_infernal(infile, evalue=1, size_max_attc=200, size_min_attc=40):
                       "pos_beg_tmp", "pos_end_tmp",
                       "sens", "evalue"]
         idx = (df.pos_beg_tmp > df.pos_end_tmp)
-        df.loc[idx, "pos_beg"] = df.loc[idx].apply(lambda x: x["pos_end_tmp"] - (length_cm - x["cm_fin"]), axis=1)
+        df.loc[idx, "pos_beg"] = df.loc[idx].apply(lambda x: x["pos_end_tmp"] - (len_model_attc - x["cm_fin"]), axis=1)
         df.loc[idx, "pos_end"] = df.loc[idx].apply(lambda x: x["pos_beg_tmp"] + (x["cm_debut"] - 1), axis=1)
 
-        df.loc[~idx, "pos_end"] = df.loc[~idx].apply(lambda x: x["pos_end_tmp"] + (length_cm - x["cm_fin"]), axis=1)
+        df.loc[~idx, "pos_end"] = df.loc[~idx].apply(lambda x: x["pos_end_tmp"] + (len_model_attc - x["cm_fin"]), axis=1)
         df.loc[~idx, "pos_beg"] = df.loc[~idx].apply(lambda x: x["pos_beg_tmp"] - (x["cm_debut"] - 1), axis=1)
 
         return df[["Accession_number", "cm_attC", "cm_debut", "cm_fin",
@@ -43,7 +46,11 @@ def read_infernal(infile, evalue=1, size_max_attc=200, size_min_attc=40):
                                      "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
 
 
-def local_max(replicon_name, window_beg, window_end, strand_search="both"):
+def local_max(replicon_name, sequence,
+              window_beg, window_end, len_model_attc,
+              model_attc='attc_4.cm', strand_search="both",
+              evalue_attc=1, max_attc_size=200, min_attc_size=40,
+              cmsearch_bin='cmsearch', out_dir='.', cpu_nb=1):
     """
 
     :param replicon_name: the name of replicon (without suffix)
@@ -57,59 +64,58 @@ def local_max(replicon_name, window_beg, window_end, strand_search="both"):
     :return:
     :rtype: :class:`pd.DataFrame` object
     """
+    replicon_size = len(sequence)
     if window_beg < window_end:
-        subseq = SEQUENCE[window_beg : window_end]
+        subseq = sequence[window_beg:window_end]
     else:
-        subseq1 = SEQUENCE[window_beg : SIZE_REPLICON]
-        subseq2 = SEQUENCE[:window_end]
+        subseq1 = sequence[window_beg:replicon_size]
+        subseq2 = sequence[:window_end]
         subseq = subseq1 + subseq2
 
     with open(os.path.join(out_dir, replicon_name + "_subseq.fst"), "w") as f:
         SeqIO.write(subseq, f, "fasta")
 
-    output_path = os.path.join(out_dir,
-                               "{name}_{win_beg}_{win_end}_subseq_attc.res".format(name=replicon_name,
-                                                                                    win_beg=window_beg,
-                                                                                    win_end=window_end))
-    tblout_path = os.path.join(out_dir,
-                                "{name}_{win_beg}_{win_end}_subseq_attc_table.res".format(name=replicon_name,
-                                                                                          win_beg=window_beg,
-                                                                                          win_end=window_end))
+    output_path = os.path.join(out_dir, "{name}_{win_beg}_{win_end}_subseq_attc.res".format(name=replicon_name,
+                                                                                            win_beg=window_beg,
+                                                                                            win_end=window_end))
+    tblout_path = os.path.join(out_dir, "{name}_{win_beg}_{win_end}_subseq_attc_table.res".format(name=replicon_name,
+                                                                                                  win_beg=window_beg,
+                                                                                                  win_end=window_end))
 
     infile_path = os.path.join(out_dir, replicon_name + "_subseq.fst")
     if strand_search == "both":
-        cmsearch_cmd = [CMSEARCH,
-                        "-Z", str(SIZE_REPLICON / 1000000.),
+        cmsearch_cmd = [cmsearch_bin,
+                        "-Z", str(replicon_size / 1000000.),
                         "--max",
-                        "--cpu", N_CPU,
+                        "--cpu", str(cpu_nb),
                         "-o", output_path,
                         "--tblout", tblout_path,
                         "-E", "10",
-                        MODEL_attc,
+                        model_attc,
                         infile_path]
 
     elif strand_search == "top":
-        cmsearch_cmd = [CMSEARCH,
-                        "-Z", str(SIZE_REPLICON / 1000000.),
+        cmsearch_cmd = [cmsearch_bin,
+                        "-Z", str(replicon_size / 1000000.),
                         "--toponly",
                         "--max",
-                        "--cpu", N_CPU,
+                        "--cpu", str(cpu_nb),
                         "-o", output_path,
                         "--tblout", tblout_path,
                         "-E", "10",
-                        MODEL_attc,
+                        model_attc,
                         infile_path]
 
     elif strand_search == "bottom":
-        cmsearch_cmd = [CMSEARCH,
-                        "-Z", str(SIZE_REPLICON / 1000000.),
+        cmsearch_cmd = [cmsearch_bin,
+                        "-Z", str(replicon_size / 1000000.),
                         "--bottomonly",
                         "--max",
-                        "--cpu", N_CPU,
+                        "--cpu", str(cpu_nb),
                         "-o", output_path,
                         "--tblout", tblout_path,
                         "-E", "10",
-                        MODEL_attc,
+                        model_attc,
                         infile_path]
     try:
         returncode = call(cmsearch_cmd)
@@ -119,15 +125,17 @@ def local_max(replicon_name, window_beg, window_end, strand_search="both"):
         raise RuntimeError("{0} failed returncode = {1}".format(cmsearch_cmd[0], returncode))
 
     df_max = read_infernal(tblout_path,
+                           replicon_name, len_model_attc,
                            evalue=evalue_attc,
                            size_max_attc=max_attc_size,
                            size_min_attc=min_attc_size)
-    df_max.pos_beg = (df_max.pos_beg + window_beg) % SIZE_REPLICON
-    df_max.pos_end = (df_max.pos_end + window_beg) % SIZE_REPLICON
+    df_max.pos_beg = (df_max.pos_beg + window_beg) % replicon_size
+    df_max.pos_end = (df_max.pos_end + window_beg) % replicon_size
     df_max.to_csv(os.path.join(out_dir, replicon_name + "_subseq_attc_table_end.res"),
                   sep="\t", index=0, mode="a", header=0)
     # filter on size
-    df_max = df_max[(abs(df_max.pos_end - df_max.pos_beg) > min_attc_size) & (abs(df_max.pos_end - df_max.pos_beg) < max_attc_size)]
+    df_max = df_max[(abs(df_max.pos_end - df_max.pos_beg) > min_attc_size) &
+                    (abs(df_max.pos_end - df_max.pos_beg) < max_attc_size)]
     return df_max
 
 
--- tests/test_read_infernal.py
+++ tests/test_read_infernal.py
@@ -6,22 +6,27 @@
 Unit tests read_infernal function of integron_finder
 """
 
-import integron_finder
-import pandas as pd
 import os
-import unittest
+import pandas as pd
 import pandas.util.testing as pdt
 
+try:
+    from tests import IntegronTest
+except ImportError as err:
+    msg = "Cannot import integron_finder: {0!s}".format(err)
+    raise ImportError(msg)
 
-class TestFunctions(unittest.TestCase):
+from integron_finder import infernal
+
+
+class TestFunctions(IntegronTest):
 
     def setUp(self):
         """
         Define variables common to all tests
         """
-        self.rep_name = "acba.007.p01.13"
-        integron_finder.replicon_name = self.rep_name  # name of replicon in given fasta file
-        integron_finder.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
+        self.replicon_name = "acba.007.p01.13"
+        self.length_cm = 47  # length in 'CLEN' (value for model attc_4.cm)
 
 
     def test_nofile(self):
@@ -31,7 +36,7 @@ class TestFunctions(unittest.TestCase):
 
         """
         filename = "infernal.txt"
-        df = integron_finder.read_infernal(filename)
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
         pdt.assert_frame_equal(df, expect)
@@ -41,9 +46,9 @@ class TestFunctions(unittest.TestCase):
         Test that if the infernal file exists but there is no hit
         inside, it returns an empty dataframe.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table-empty.res")
-        df = integron_finder.read_infernal(filename)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table-empty.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
         pdt.assert_frame_equal(df, expect)
@@ -54,9 +59,9 @@ class TestFunctions(unittest.TestCase):
         the given evalue threshold is smaller than the hits thresholds:
         no hit kept, should return an empty dataframe.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, evalue=1e-10)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, evalue=1e-10)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
         pdt.assert_frame_equal(df, expect)
@@ -66,20 +71,20 @@ class TestFunctions(unittest.TestCase):
         Test that if the infernal file exists and there are hits, it returns the
         dataframe corresponding to it.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19618,
                                 "pos_end": 19726, "sens": "-", "evalue": 1.1e-7},
                                ignore_index=True)
@@ -92,16 +97,16 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a minimum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, size_min_attc=60)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, size_min_attc=60)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19618,
                                 "pos_end": 19726, "sens": "-", "evalue": 1.1e-7},
                                ignore_index=True)
@@ -114,16 +119,16 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a maximum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, size_max_attc=100)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, size_max_attc=100)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
@@ -136,12 +141,12 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a maximum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, evalue=1e-8)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, evalue=1e-8)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
@@ -155,20 +160,20 @@ class TestFunctions(unittest.TestCase):
         Test that when the model did not completely match on the sequence,
         the start and end positions of hit are well recalculated. All hits are on strand +
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table-partial.res")
-        df = integron_finder.read_infernal(filename)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table-partial.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 40, "pos_beg": 17825,
                                 "pos_end": 17891, "sens": "+", "evalue": 1e-9},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "+", "evalue": 1e-4},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 10, "cm_fin": 47, "pos_beg": 19609,
                                 "pos_end": 19726, "sens": "+", "evalue": 1.1e-7},
                                ignore_index=True)
@@ -182,20 +187,20 @@ class TestFunctions(unittest.TestCase):
         Test that when the model did not completely match on the sequence,
         the start and end positions of hit are well recalculated. All hits are on strand -
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table-partialm.res")
-        df = integron_finder.read_infernal(filename)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table-partialm.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 40, "pos_beg": 17818,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 10, "cm_fin": 47, "pos_beg": 19618,
                                 "pos_end": 19735, "sens": "-", "evalue": 1.1e-7},
                                ignore_index=True)
@@ -208,16 +213,16 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a minimum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, size_min_attc=60)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, size_min_attc=60)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19618,
                                 "pos_end": 19726, "sens": "-", "evalue": 1.1e-7},
                                ignore_index=True)
@@ -230,16 +235,16 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a maximum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, size_max_attc=100)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, size_max_attc=100)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 19080,
                                 "pos_end": 19149, "sens": "-", "evalue": 1e-4},
                                ignore_index=True)
@@ -252,12 +257,12 @@ class TestFunctions(unittest.TestCase):
         """
         Test that the filter by a maximum attc size works.
         """
-        filename = os.path.join("tests", "data", "Results_Integron_Finder_" + self.rep_name,
-                                 "other", self.rep_name + "_attc_table.res")
-        df = integron_finder.read_infernal(filename, evalue=1e-8)
+        filename = self.find_data(os.path.join("Results_Integron_Finder_" + self.replicon_name,
+                                  "other", self.replicon_name + "_attc_table.res"))
+        df = infernal.read_infernal(filename, self.replicon_name, self.length_cm, evalue=1e-8)
         expect = pd.DataFrame(columns=["Accession_number", "cm_attC", "cm_debut",
                                        "cm_fin", "pos_beg", "pos_end", "sens", "evalue"])
-        expect = expect.append({"Accession_number": self.rep_name, "cm_attC": "attC_4",
+        expect = expect.append({"Accession_number": self.replicon_name, "cm_attC": "attC_4",
                                 "cm_debut": 1, "cm_fin": 47, "pos_beg": 17825,
                                 "pos_end": 17884, "sens": "-", "evalue": 1e-9},
                                ignore_index=True)
