2e6039a0b2a4cc1eb4d1dce0f6e8f6f67efd6ef9
Bertrand Néron <bneron@pasteur.fr>
2018-02-21 11:55:55 +0100

    fix fake_local_max according to new api

--- tests/test_expand.py
+++ tests/test_expand.py
@@ -24,20 +24,26 @@ _local_max_ori = infernal.local_max
 
 def local_max_mock():
     """expand call several time local_max which run cmserach and need lot of global variable
-    to avoid to call cmsearch (we don't want to test local max), I have been ccahed the results
+    to avoid to call cmsearch (we don't want to test local max), I have been cached the results
     of local_max and replace the function by this mock
     """
-    _cache = {('lian.001.c02.10', 942899, 947099, 'top'):
+    _cache = {('lian.001.c02.10', 942899, 947099, 'attc_4', 'top'):
                   pd.read_csv(TestExpand.find_data('local_max_right_circular_1.csv')),
-              ('lian.001.c02.10', 946899, 951099, 'top'):
+              ('lian.001.c02.10', 946899, 951099, 'attc_4', 'top'):
                   pd.read_csv(TestExpand.find_data('local_max_right_circular_2.csv')),
-              ('lian.001.c02.10', 930689, 934889, 'bottom'):
+              ('lian.001.c02.10', 930689, 934889, 'attc_4', 'bottom'):
                   pd.read_csv(TestExpand.find_data('local_max_left_circular.csv')),
-              ('lian.001.c02.10', 930689, 930889, 'bottom'):
+              ('lian.001.c02.10', 930689, 930889, 'attc_4', 'bottom'):
                   pd.read_csv(TestExpand.find_data('local_max_left_linear.csv'))
               }
 
-    def fake_local_max(*args):
+    def fake_local_max(*args, **kwargs):
+        replicon, window_beg, window_end, model_attc_path = args
+        fake_args = (replicon.name,
+                     window_beg, window_end,
+                     os.path.splitext(os.path.split(model_attc_path)[1])[0],
+                     kwargs['strand_search'])
+        args = tuple(fake_args)
         return _cache[args]
     return fake_local_max
 
@@ -55,6 +61,7 @@ class TestExpand(IntegronTest):
         self.tmp_dir = os.path.join(tempfile.gettempdir(), 'tmp_test_integron_finder')
         os.makedirs(self.tmp_dir)
 
+        self.model_attc_path = self.find_data(os.path.join('Models', 'attc_4.cm'))
         infernal.local_max = local_max_mock()
 
 
@@ -73,14 +80,15 @@ class TestExpand(IntegronTest):
         max_attc_size = 200
 
         replicon_path = self.find_data(os.path.join('Replicons', replicon_name + '.fst'))
-        sequence = read_fasta(replicon_path)
+        replicon = read_fasta(replicon_path)
 
         max_elt_input = pd.read_csv(os.path.join(self._data_dir, 'max_elt_input_1.csv'))
         df_max_input = pd.read_csv(os.path.join(self._data_dir, 'df_max_input_1.csv'))
         max_elt_expected = pd.read_csv(os.path.join(self._data_dir, 'max_elt_output_lian_right.csv'))
-        max_eat_received = infernal.expand(replicon_name, len(sequence),
+        max_eat_received = infernal.expand(replicon,
                                            934689, 943099, max_elt_input, df_max_input,
                                            circular, dist_threshold, max_attc_size,
+                                           self.model_attc_path,
                                            search_left=False, search_right=True)
         pdt.assert_frame_equal(max_elt_expected, max_eat_received)
 
@@ -92,14 +100,15 @@ class TestExpand(IntegronTest):
         max_attc_size = 200
 
         replicon_path = self.find_data(os.path.join('Replicons', replicon_name + '.fst'))
-        sequence = read_fasta(replicon_path)
+        replicon = read_fasta(replicon_path)
 
         max_elt_input = pd.read_csv(os.path.join(self._data_dir, 'max_elt_input_1.csv'))
         df_max_input = pd.read_csv(os.path.join(self._data_dir, 'df_max_input_1.csv'))
         max_elt_expected = pd.read_csv(os.path.join(self._data_dir, 'max_elt_output_lian_left.csv'))
-        max_eat_received = infernal.expand(replicon_name, len(sequence),
+        max_eat_received = infernal.expand(replicon,
                                            934689, 943099, max_elt_input, df_max_input,
                                            circular, dist_threshold, max_attc_size,
+                                           self.model_attc_path,
                                            search_left=True, search_right=False)
         pdt.assert_frame_equal(max_elt_expected, max_eat_received)
 
@@ -111,14 +120,15 @@ class TestExpand(IntegronTest):
         max_attc_size = 200
 
         replicon_path = self.find_data(os.path.join('Replicons', replicon_name + '.fst'))
-        sequence = read_fasta(replicon_path)
+        replicon = read_fasta(replicon_path)
 
         max_elt_input = pd.read_csv(os.path.join(self._data_dir, 'max_elt_input_1.csv'))
         df_max_input = pd.read_csv(os.path.join(self._data_dir, 'df_max_input_1.csv'))
         max_elt_expected = pd.read_csv(os.path.join(self._data_dir, 'max_elt_output_lian_right.csv'))
-        max_eat_received = infernal.expand(replicon_name, len(sequence),
+        max_eat_received = infernal.expand(replicon,
                                            934689, 943099, max_elt_input, df_max_input,
                                            circular, dist_threshold, max_attc_size,
+                                           self.model_attc_path,
                                            search_left=False, search_right=True)
         pdt.assert_frame_equal(max_elt_expected, max_eat_received)
 
@@ -130,13 +140,14 @@ class TestExpand(IntegronTest):
         max_attc_size = 200
 
         replicon_path = self.find_data(os.path.join('Replicons', replicon_name + '.fst'))
-        sequence = read_fasta(replicon_path)
+        replicon = read_fasta(replicon_path)
 
         max_elt_input = pd.read_csv(os.path.join(self._data_dir, 'max_elt_input_1.csv'))
         df_max_input = pd.read_csv(os.path.join(self._data_dir, 'df_max_input_1.csv'))
         max_elt_expected = pd.read_csv(os.path.join(self._data_dir, 'max_elt_output_lian_left.csv'))
-        max_eat_received = infernal.expand(replicon_name, len(sequence),
+        max_eat_received = infernal.expand(replicon,
                                            934689, 943099, max_elt_input, df_max_input,
                                            circular, dist_threshold, max_attc_size,
+                                           self.model_attc_path,
                                            search_left=True, search_right=False)
         pdt.assert_frame_equal(max_elt_expected, max_eat_received)
