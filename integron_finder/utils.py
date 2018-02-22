bc0cf92235c8a772195f5e6c6ed81475c723d899
Bertrand Néron <bneron@pasteur.fr>
2018-02-22 15:26:35 +0100

    change read_fasta for more generic fasta_reader

--- integron_finder/utils.py
+++ integron_finder/utils.py
@@ -2,25 +2,35 @@ import os
 from Bio import Seq
 from Bio import SeqIO
 
-def read_fasta(path):
-    """
-    :param path:the path to the fasta file
-    :return: the sequence parsed
-    :rtype: :class:`Bio.SeqRecord.SeqRecord` object
-    """
-    seq = SeqIO.read(path, "fasta",  alphabet=Seq.IUPAC.unambiguous_dna)
-    seq.name = get_name_from_path(path)
-    return seq
+
+def make_single_fasta_reader(alphabet):
+
+    def read_fasta(path):
+        """
+        :param path:the path to the fasta file
+        :return: the sequence parsed
+        :rtype: :class:`Bio.SeqRecord.SeqRecord` object
+        """
+        seq = SeqIO.read(path, "fasta",  alphabet=alphabet)
+        seq.name = get_name_from_path(path)
+        return seq
+    return read_fasta
+
+
+read_single_dna_fasta = make_single_fasta_reader(Seq.IUPAC.unambiguous_dna)
+
+
+read_single_prot_fasta = make_single_fasta_reader(Seq.IUPAC.protein)
 
 
 def model_len(path):
     if not os.path.exists(path):
-        raise RuntimeError("Path to model_attc '{}' doe snot exists".format(path))
+        raise IOError("Path to model_attc '{}' does snot exists".format(path))
     with open(path) as model_file:
         for line in model_file:
             if line.startswith('CLEN'):
-                model_len = int(line.split()[1])
-                return model_len
+                model_length = int(line.split()[1])
+                return model_length
         raise RuntimeError("CLEN not found in '{}', maybe it's not infernal model file".format(path))
 
 
