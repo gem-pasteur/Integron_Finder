d501547a57bc5ebf9c236f585b5d8a2e4fa79f9f
Bertrand Néron <bneron@pasteur.fr>
2018-02-22 15:24:59 +0100

    :white_check_mark: add unit tests for utils module

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,3 +22,23 @@ class TestUtils(IntegronTest):
         self.assertTrue(replicon.seq.endswith('CGACCCACGGCGTAACGCGCT'))
 
 
+    def test_model_len(self):
+        model_path = self.find_data(os.path.join('Models', 'attc_4.cm'))
+        self.assertEqual(utils.model_len(model_path), 47)
+        bad_path = 'nimportnaoik'
+        with self.assertRaises(IOError) as ctx:
+            utils.model_len(bad_path)
+        self.assertEqual(str(ctx.exception),
+                         "Path to model_attc '{}' does snot exists".format(bad_path))
+        bad_path = self.find_data(os.path.join('Models', 'phage-int.hmm'))
+        with self.assertRaises(RuntimeError) as ctx:
+            utils.model_len(bad_path)
+        self.assertEqual(str(ctx.exception),
+                         "CLEN not found in '{}', maybe it's not infernal model file".format(bad_path))
+
+
+    def test_get_name_from_path(self):
+        self.assertEqual(utils.get_name_from_path('/foo/bar.baz'), 'bar')
+        self.assertEqual(utils.get_name_from_path('bar.baz'), 'bar')
+        self.assertEqual(utils.get_name_from_path('../foo/bar.baz'), 'bar')
+        self.assertEqual(utils.get_name_from_path('../foo/bar'), 'bar')
