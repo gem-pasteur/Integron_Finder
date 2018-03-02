f35777fa5fe9cbaf697ae767fb13c0c2b2595262
Bertrand Néron <bneron@pasteur.fr>
2018-03-02 14:46:50 +0100

    :pencil2: :art: witer api doc change api of to_gbk
    change name of to_gbk function as it does not generate a genbank
    sequence. it just add integrons annotation to the SeqRecord.
    
    change order of parameters
    
    wite documentation

--- integron_finder/genbank.py
+++ integron_finder/genbank.py
@@ -32,9 +32,17 @@ from Bio import SeqFeature
 from Bio import SeqIO
 
 
-def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
+def add_feature(replicon, integron_desc, prot_file, dist_threshold):
+    """
+    Add integron annotation to the replicon.
 
-    """ from a dataframe like integrons_describe and a sequence, create an genbank file with integron annotation """
+    :param replicon: The Replicon to annotate
+    :type replicon: a :class:`Bio.Seq.SeqRecord` object.
+    :param integron_desc:
+    :type integron_desc: a :class:`pandas.DataFrame`
+    :param prot_file:
+    :param int dist_threshold:
+    """
 
     integron_desc = integron_desc.set_index("ID_integron").copy()
     for i in integron_desc.index.unique():
@@ -48,7 +56,7 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
                                         type="integron",
                                         qualifiers={"integron_id": i, "integron_type": type_integron}
                                         )
-            sequence.features.append(tmp)
+            replicon.features.append(tmp)
             if integron_desc.loc[i].type_elt == "protein":
 
                 tmp = SeqFeature.SeqFeature(location=SeqFeature.FeatureLocation(integron_desc.loc[i].pos_beg - 1,
@@ -62,7 +70,7 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
 
                 tmp.qualifiers["translation"] = [prt for prt in SeqIO.parse(prot_file, "fasta")
                                                  if prt.id == integron_desc.loc[i].element][0].seq
-                sequence.features.append(tmp)
+                replicon.features.append(tmp)
 
             else:
                 tmp = SeqFeature.SeqFeature(location=SeqFeature.FeatureLocation(integron_desc.loc[i].pos_beg - 1,
@@ -73,17 +81,17 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
                                                         "model": integron_desc.loc[i].model}
                                             )
 
-                sequence.features.append(tmp)
+                replicon.features.append(tmp)
 
         else:
             type_integron = integron_desc.loc[i].type.values[0]
-            # Should only be true if integron over edge of sequence:
+            # Should only be true if integron over edge of replicon:
             diff = integron_desc.loc[i].pos_beg.diff() > dist_threshold
 
             if diff.any():
                 pos = np.where(diff)[0][0]
                 start_integron_1 = integron_desc.loc[i].pos_beg.values[pos]
-                end_integron_1 = len(sequence)
+                end_integron_1 = len(replicon)
                 start_integron_2 = 1
                 end_integron_2 = integron_desc.loc[i].pos_end.values[pos-1]
 
@@ -103,7 +111,7 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
                                             type="integron",
                                             qualifiers={"integron_id": i, "integron_type": type_integron}
                                             )
-            sequence.features.append(tmp)
+            replicon.features.append(tmp)
             for r in integron_desc.loc[i].iterrows():
                 if r[1].type_elt == "protein":
                     tmp = SeqFeature.SeqFeature(location=SeqFeature.FeatureLocation(r[1].pos_beg - 1, r[1].pos_end),
@@ -116,7 +124,7 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
 
                     tmp.qualifiers["translation"] = [prt for prt in SeqIO.parse(prot_file, "fasta")
                                                      if prt.id == r[1].element][0].seq
-                    sequence.features.append(tmp)
+                    replicon.features.append(tmp)
                 else:
                     tmp = SeqFeature.SeqFeature(location=SeqFeature.FeatureLocation(r[1].pos_beg - 1, r[1].pos_end),
                                                 strand=r[1].strand,
@@ -124,9 +132,9 @@ def to_gbk(integron_desc, sequence, prot_file, dist_threshold):
                                                 qualifiers={r[1].type_elt: r[1].element, "model": r[1].model}
                                                 )
 
-                    sequence.features.append(tmp)
+                    replicon.features.append(tmp)
 
     # We get a ValueError otherwise, eg:
     # ValueError: Locus identifier 'gi|00000000|gb|XX123456.2|' is too long
-    if len(sequence.name) > 16:
-        sequence.name = sequence.name[-16:]
+    if len(replicon.name) > 16:
+        replicon.name = replicon.name[-16:]
