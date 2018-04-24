d59912db2e05757076e788e452bb7efa0fd4dc80
Bertrand Néron <bneron@pasteur.fr>
2018-04-24 15:33:40 +0200

    :bug: fix test merge_results dbtype was wrong

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -182,7 +182,7 @@ sequence seq_(4|2) is too short \(32 bp\), the sequence is skipped \(must be > 5
                  "ID_replicon": 'str',
                  "element": 'str',
                  "pos_beg": 'int',
-                 "pos_beg": 'int',
+                 "pos_end": 'int',
                  "strand": 'int',
                  "evalue": 'float',
                  "type_elt": 'str',
@@ -195,39 +195,39 @@ sequence seq_(4|2) is too short \(32 bp\), the sequence is skipped \(must be > 5
                  }
         try:
             res1 = pd.DataFrame({"ID_integron": ['integron_01'],
-                                "ID_replicon": ['ACBA.007.P01_13'],
-                                "element": ['Pc_int1'],
-                                "pos_beg": [25],
-                                "pos_end": [51],
-                                "strand": [-1],
-                                "evalue": [None],
-                                "type_elt": ['Promoter'],
-                                "annotation": ['Pc_1'],
-                                "model": [None],
-                                "type": ['complete'],
-                                "default": ['No'],
-                                "distance_2attC": [None],
-                                "considered_topology": ['lin'],
-                                })
+                                 "ID_replicon": ['ACBA.007.P01_13'],
+                                 "element": ['Pc_int1'],
+                                 "pos_beg": [25],
+                                 "pos_end": [51],
+                                 "strand": [-1],
+                                 "evalue": [None],
+                                 "type_elt": ['Promoter'],
+                                 "annotation": ['Pc_1'],
+                                 "model": [None],
+                                 "type": ['complete'],
+                                 "default": ['No'],
+                                 "distance_2attC": [None],
+                                 "considered_topology": ['lin'],
+                                 })
             res1 = res1.astype(dtype=dtype)
             res1.to_csv(f1, sep="\t", index=False, na_rep="NA")
 
             f2 = os.path.join(tmp_dir, 'f2')
             res2 = pd.DataFrame({"ID_integron": ['integron_02'],
-                                "ID_replicon": ['LIAN.001.C02_10'],
-                                "element": ['LIAN.001.C02_10_825'],
-                                "pos_beg": [934165],
-                                "pos_end": [934689],
-                                "strand": [-1],
-                                "evalue": [1.9e-26],
-                                "type_elt": ['protein intI'],
-                                "annotation": ['intersection_tyr_intI'],
-                                "model": [None],
-                                "type": ['complete'],
-                                "default": ['No'],
-                                "distance_2attC": [None],
-                                "considered_topology": ['lin']
-                                })
+                                 "ID_replicon": ['LIAN.001.C02_10'],
+                                 "element": ['LIAN.001.C02_10_825'],
+                                 "pos_beg": [934165],
+                                 "pos_end": [934689],
+                                 "strand": [-1],
+                                 "evalue": [1.9e-26],
+                                 "type_elt": ['protein intI'],
+                                 "annotation": ['intersection_tyr_intI'],
+                                 "model": [None],
+                                 "type": ['complete'],
+                                 "default": ['No'],
+                                 "distance_2attC": [None],
+                                 "considered_topology": ['lin']
+                                 })
             res2 = res2.astype(dtype=dtype)
             res2.to_csv(f2, sep="\t", index=False, na_rep="NA")
 
