c474331851d6a68ddef6d06d2fa2e77d93d86afb
Bertrand Néron <bneron@pasteur.fr>
2018-11-30 11:17:33 +0100

    :white_check_mark: add or modify test to use gembase new version

--- tests/test_read_hmm.py
+++ tests/test_read_hmm.py
@@ -48,8 +48,6 @@ class TestReadHMM(IntegronTest):
         """
         Define variables common to all tests
         """
-        self.rep_name = "acba.007.p01.13"
-        self.replicon_id = 'ACBA.007.P01_13'
         # Simulate argparse to get argument
         args = argparse.Namespace()
         args.gembase = False
@@ -60,8 +58,11 @@ class TestReadHMM(IntegronTest):
         Test that when there are no hits in the hmm result file, it returns an empty
         dataframe, without error.
         """
-        infile = self.find_data(os.path.join("fictive_results", "{}_intI-empty.res".format(self.replicon_id)))
-        df = read_hmm(self.rep_name, infile, self.cfg)
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        
+        infile = self.find_data(os.path.join("fictive_results", "{}_intI-empty.res".format(replicon_id)))
+        df = read_hmm(rep_name, infile, self.cfg)
         exp = pd.DataFrame(columns=["Accession_number", "query_name", "ID_query", "ID_prot",
                                     "strand", "pos_beg", "pos_end", "evalue"])
 
@@ -75,11 +76,13 @@ class TestReadHMM(IntegronTest):
         """
         Test that the hmm hits are well read
         """
-        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(self.rep_name),
-                                             "tmp_{}".format(self.replicon_id),
-                                             "{}_intI.res".format(self.replicon_id)))
-        df = read_hmm(self.rep_name, infile, self.cfg)
-        exp = pd.DataFrame(data={"Accession_number": self.rep_name, "query_name": "intI_Cterm",
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(rep_name),
+                                             "tmp_{}".format(replicon_id),
+                                             "{}_intI.res".format(replicon_id)))
+        df = read_hmm(rep_name, infile, self.cfg)
+        exp = pd.DataFrame(data={"Accession_number": rep_name, "query_name": "intI_Cterm",
                                  "ID_query": "-", "ID_prot": "ACBA.007.P01_13_1", "strand": 1,
                                  "pos_beg": 55, "pos_end": 1014, "evalue": 1.9e-25},
                            index=[0])
@@ -93,15 +96,18 @@ class TestReadHMM(IntegronTest):
         Test that the hmm hits are well read, when the gembase format is used (.prt file is
         provided, prodigal is not used to find the proteins).
         """
-
-        infile = self.find_data(os.path.join("fictive_results", "{}_intI-gembase.res".format(self.replicon_id)))
+        replicon_id = 'ACBA.0917.00019'
+        contig_id = 'ACBA.0917.00019.0001'
+        infile = self.find_data(os.path.join("Results_Integron_Finder_{}.gembase".format(replicon_id),
+                                             "tmp_{}".format(contig_id),
+                                             "{}_intI.res".format(contig_id)))
 
         args = argparse.Namespace()
         args.gembase = True
         cfg = Config(args)
 
-        df = read_hmm(self.replicon_id, infile, cfg)
-        exp = pd.DataFrame(data={"Accession_number": self.replicon_id,
+        df = read_hmm(contig_id, infile, cfg)
+        exp = pd.DataFrame(data={"Accession_number": replicon_id,
                                  "query_name": "intI_Cterm",
                                  "ID_query": "-",
                                  "ID_prot": "ACBA007p01a_000009",
@@ -120,18 +126,20 @@ class TestReadHMM(IntegronTest):
         Test that the hmm hits are well read, and returned only if evalue is < to the
         given threshold.
         """
-        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(self.rep_name),
-                                             "tmp_{}".format(self.replicon_id),
-                                             "{}_intI.res".format(self.replicon_id)))
-        df1 = read_hmm(self.rep_name, infile, self.cfg, evalue=1.95e-25)
-        exp1 = pd.DataFrame(data={"Accession_number": self.rep_name, "query_name": "intI_Cterm",
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(rep_name),
+                                             "tmp_{}".format(replicon_id),
+                                             "{}_intI.res".format(replicon_id)))
+        df1 = read_hmm(rep_name, infile, self.cfg, evalue=1.95e-25)
+        exp1 = pd.DataFrame(data={"Accession_number": rep_name, "query_name": "intI_Cterm",
                                   "ID_query": "-", "ID_prot": "ACBA.007.P01_13_1", "strand": 1,
                                   "pos_beg": 55, "pos_end": 1014, "evalue": 1.9e-25},
                             index=[0])
         exp1 = exp1[["Accession_number", "query_name", "ID_query", "ID_prot",
                      "strand", "pos_beg", "pos_end", "evalue"]]
         pdt.assert_frame_equal(df1, exp1)
-        df2 = read_hmm(self.replicon_id, infile, self.cfg, evalue=1.9e-25)
+        df2 = read_hmm(replicon_id, infile, self.cfg, evalue=1.9e-25)
         exp2 = pd.DataFrame(columns=["Accession_number", "query_name", "ID_query", "ID_prot",
                                      "strand", "pos_beg", "pos_end", "evalue"])
 
@@ -147,10 +155,12 @@ class TestReadHMM(IntegronTest):
         Test that the hmm hits are well read, it returns only the hits with evalue < given
         threshold
         """
-        infile = self.find_data(os.path.join("fictive_results", "{}_intI.res".format(self.replicon_id)))
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("fictive_results", "{}_intI.res".format(replicon_id)))
 
-        df1 = read_hmm(self.replicon_id, infile, self.cfg, evalue=1e-3)
-        exp1 = pd.DataFrame(data={"Accession_number": [self.replicon_id] * 2,
+        df1 = read_hmm(replicon_id, infile, self.cfg, evalue=1e-3)
+        exp1 = pd.DataFrame(data={"Accession_number": [replicon_id] * 2,
                                   "query_name": ["intI_Cterm"] * 2,
                                   "ID_query": ["-", "-"],
                                   "ID_prot": ["ACBA.007.P01_13_1", "ACBA.007.P01_13_3"],
@@ -168,18 +178,20 @@ class TestReadHMM(IntegronTest):
         Test that the hmm hits are well read, and returned only if coverage is > to the
         given threshold.
         """
-        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(self.rep_name),
-                                             "tmp_{}".format(self.replicon_id),
-                                             "{}_intI.res".format(self.replicon_id)))
-        df1 = read_hmm(self.rep_name, infile, self.cfg, coverage=0.945)
-        exp1 = pd.DataFrame(data={"Accession_number": self.rep_name, "query_name": "intI_Cterm",
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("Results_Integron_Finder_{}".format(rep_name),
+                                             "tmp_{}".format(replicon_id),
+                                             "{}_intI.res".format(replicon_id)))
+        df1 = read_hmm(rep_name, infile, self.cfg, coverage=0.945)
+        exp1 = pd.DataFrame(data={"Accession_number": rep_name, "query_name": "intI_Cterm",
                                   "ID_query": "-", "ID_prot": "ACBA.007.P01_13_1", "strand": 1,
                                   "pos_beg": 55, "pos_end": 1014, "evalue": 1.9e-25},
                             index=[0])
         exp1 = exp1[["Accession_number", "query_name", "ID_query", "ID_prot",
                      "strand", "pos_beg", "pos_end", "evalue"]]
         pdt.assert_frame_equal(df1, exp1)
-        df2 = read_hmm(self.rep_name, infile, self.cfg, coverage=0.95)
+        df2 = read_hmm(rep_name, infile, self.cfg, coverage=0.95)
         exp2 = pd.DataFrame(columns=["Accession_number", "query_name", "ID_query", "ID_prot",
                                      "strand", "pos_beg", "pos_end", "evalue"])
         intcols = ["pos_beg", "pos_end", "strand"]
@@ -194,9 +206,11 @@ class TestReadHMM(IntegronTest):
         Test that the hmm hits are well read, it returns only the hits with coverage >
         given threshold
         """
-        infile = self.find_data(os.path.join("fictive_results", "{}_intI.res".format(self.replicon_id)))
-        df1 = read_hmm(self.rep_name, infile, self.cfg, coverage=0.7)
-        exp1 = pd.DataFrame(data={"Accession_number": [self.rep_name] * 2,
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("fictive_results", "{}_intI.res".format(replicon_id)))
+        df1 = read_hmm(rep_name, infile, self.cfg, coverage=0.7)
+        exp1 = pd.DataFrame(data={"Accession_number": [rep_name] * 2,
                                   "query_name": ["intI_Cterm"] * 2,
                                   "ID_query": ["-", "-"],
                                   "ID_prot": ["ACBA.007.P01_13_1", "ACBA.007.P01_13_2"],
@@ -213,14 +227,16 @@ class TestReadHMM(IntegronTest):
         Test reading hmm results when there are multiple hits: 2 hits on the same protein: keep
         only the one with the best evalue. 2 hits on 2 different proteins: keep the 2 proteins.
         """
-        infile = self.find_data(os.path.join("fictive_results", "{}_intI-multi.res".format(self.replicon_id)))
+        rep_name = "acba.007.p01.13"
+        replicon_id = 'ACBA.007.P01_13'
+        infile = self.find_data(os.path.join("fictive_results", "{}_intI-multi.res".format(replicon_id)))
 
         args = argparse.Namespace()
         args.gembase = True
         cfg = Config(args)
 
-        df = read_hmm(self.rep_name, infile, cfg)
-        exp = pd.DataFrame(data={"Accession_number": [self.rep_name] * 2,
+        df = read_hmm(rep_name, infile, cfg)
+        exp = pd.DataFrame(data={"Accession_number": [rep_name] * 2,
                                  "query_name": ["intI_Cterm"] * 2, "ID_query": ["-"] * 2,
                                  "ID_prot": ["ACBA007p01a_000009", "ACBA007p01a_000008"],
                                  "strand": [1, -1],
