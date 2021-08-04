d552fa7ff5b87eeb728fcad99a7fde7c6743546e
Bertrand Néron <bneron@pasteur.fr>
2021-08-04 14:00:02 +0200

    :white_check_mark: fix test as parser check path
    the argument parser now check if path given as argument exists
    so provide wahtever value but of an existing path

--- tests/test_parse_args.py
+++ tests/test_parse_args.py
@@ -41,10 +41,14 @@ from integron_finder.scripts.finder import parse_args
 
 class TestParseArgs(IntegronTest):
 
+    def setUp(self):
+        self.replicon = self.find_data('Replicons', 'acba_short.fst')
+
     def test_replicon(self):
-        replicon = 'foo'
-        cfg = parse_args([replicon])
-        self.assertEqual(cfg.input_seq_path, os.path.abspath(replicon))
+        # whatever the replicon the path must exists
+        # otherwise the parser raise an error as it check the path
+        cfg = parse_args([self.replicon])
+        self.assertEqual(cfg.input_seq_path, self.replicon)
 
     def test_wo_replicon(self):
         real_exit = sys.exit
@@ -63,136 +67,136 @@ class TestParseArgs(IntegronTest):
                 sys.exit = real_exit
 
     def test_local_max(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.local_max)
-        cfg = parse_args(['--local-max', 'replicon'])
+        cfg = parse_args(['--local-max', self.replicon])
         self.assertTrue(cfg.local_max)
 
     def test_func_annot(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.func_annot)
-        cfg = parse_args(['--func-annot', 'replicon'])
+        cfg = parse_args(['--func-annot', self.replicon])
         self.assertTrue(cfg.func_annot)
 
     def test_cpu(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.cpu, 1)
         cpu = 10
-        cfg = parse_args(['--cpu', str(cpu), 'replicon'])
+        cfg = parse_args(['--cpu', str(cpu), self.replicon])
         self.assertEqual(cfg.cpu, cpu)
 
     def test_distance_threshold(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.distance_threshold, 4000)
         dt = 50
-        cfg = parse_args(['--distance-thresh', str(dt), 'replicon'])
+        cfg = parse_args(['--distance-thresh', str(dt), self.replicon])
         self.assertEqual(cfg.distance_threshold, dt)
 
     def test_outdir(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.outdir, os.path.abspath('.'))
         outdir = 'foo'
-        cfg = parse_args(['--outdir', outdir, 'replicon'])
+        cfg = parse_args(['--outdir', outdir, self.replicon])
         self.assertEqual(cfg.outdir, os.path.abspath(outdir))
 
     def test_union_integrase(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.union_integrases)
-        cfg = parse_args(['--union-integrases', 'replicon'])
+        cfg = parse_args(['--union-integrases', self.replicon])
         self.assertTrue(cfg.union_integrases)
 
     def test_cmsearch(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.cmsearch, distutils.spawn.find_executable("cmsearch"))
-        cmsearch = 'foo'
-        cfg = parse_args(['--cmsearch', cmsearch, 'replicon'])
+        cmsearch = self.find_data('Replicons', 'fake_seq.fst')  # whatever the value the file must exists
+        cfg = parse_args(['--cmsearch', cmsearch, self.replicon])
         self.assertEqual(cfg.cmsearch, cmsearch)
 
     def test_hmmsearch(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.hmmsearch, distutils.spawn.find_executable("hmmsearch"))
-        hmmsearch = 'foo'
-        cfg = parse_args(['--hmmsearch', hmmsearch, 'replicon'])
+        hmmsearch = self.find_data('Replicons', 'fake_seq.fst')  # whatever the value the file must exists
+        cfg = parse_args(['--hmmsearch', hmmsearch, self.replicon])
         self.assertEqual(cfg.hmmsearch, hmmsearch)
 
     def test_prodigal(self):
-        cfg = parse_args(['replicon'])
-        prodigal = 'foo'
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.prodigal, distutils.spawn.find_executable('prodigal'))
-        cfg = parse_args(['--prodigal', prodigal, 'replicon'])
+        prodigal = self.find_data('Replicons', 'fake_seq.fst')  # whatever the value the file must exists
+        cfg = parse_args(['--prodigal', prodigal, self.replicon])
         self.assertEqual(cfg.prodigal, prodigal)
 
     def test_path_func_annot(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertIsNone(cfg.path_func_annot)
-        func_annot = 'foo'
-        cfg = parse_args(['--path-func-annot', func_annot, 'replicon'])
+        func_annot = self.find_data('hmm_files')  # whatever the value the file must exists
+        cfg = parse_args(['--path-func-annot', func_annot, self.replicon])
         self.assertEqual(cfg.path_func_annot, func_annot)
 
     def test_gembase(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.gembase)
-        cfg = parse_args(['--gembase', 'replicon'])
+        cfg = parse_args(['--gembase', self.replicon])
         self.assertTrue(cfg.gembase)
 
     def test_attc_model(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.model_attc_name, 'attc_4')
         model = 'foo'
-        cfg = parse_args(['--attc-model', model, 'replicon'])
+        cfg = parse_args(['--attc-model', model, self.replicon])
         self.assertEqual(cfg.model_attc_name, model)
 
     def test_evalues_attc(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.evalue_attc, 1.0)
         evalue = 5.0
-        cfg = parse_args(['--evalue-attc', str(evalue), 'replicon'])
+        cfg = parse_args(['--evalue-attc', str(evalue), self.replicon])
         self.assertEqual(cfg.evalue_attc, evalue)
 
     def test_keep_palindromes(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.keep_palindromes)
-        cfg = parse_args(['--keep-palindromes', 'replicon'])
+        cfg = parse_args(['--keep-palindromes', self.replicon])
         self.assertTrue(cfg.keep_palindromes)
 
     def test_no_proteins(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.no_proteins)
-        cfg = parse_args(['--no-proteins', 'replicon'])
+        cfg = parse_args(['--no-proteins', self.replicon])
         self.assertTrue(cfg.no_proteins)
 
     def test_max_attc_size(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.max_attc_size, 200)
         max_attc_size = 50
-        cfg = parse_args(['--max-attc-size', str(max_attc_size), 'replicon'])
+        cfg = parse_args(['--max-attc-size', str(max_attc_size), self.replicon])
         self.assertEqual(cfg.max_attc_size, max_attc_size)
 
 
     def test_min_attc_size(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.min_attc_size, 40)
         min_attc_size = 50
-        cfg = parse_args(['--min-attc-size', str(min_attc_size), 'replicon'])
+        cfg = parse_args(['--min-attc-size', str(min_attc_size), self.replicon])
         self.assertEqual(cfg.min_attc_size, min_attc_size)
 
     def test_eagle_eyes(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.local_max)
         # eagle option is an "alias" for local_max
-        cfg = parse_args(['--eagle-eyes', 'replicon'])
+        cfg = parse_args(['--eagle-eyes', self.replicon])
         self.assertTrue(cfg.local_max)
 
     def test_circular(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.circular)
-        cfg = parse_args(['--circ', 'replicon'])
+        cfg = parse_args(['--circ', self.replicon])
         self.assertTrue(cfg.circular)
 
     def test_linear(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.linear)
-        cfg = parse_args(['--linear', 'replicon'])
+        cfg = parse_args(['--linear', self.replicon])
         self.assertTrue(cfg.linear)
 
     def test_cirular_n_linear(self):
@@ -201,7 +205,7 @@ class TestParseArgs(IntegronTest):
         sys.exit = self.fake_exit
         with self.catch_io(err=True):
             try:
-                _ = parse_args(['--circ', '--linear', 'replicon'])
+                _ = parse_args(['--circ', '--linear', self.replicon])
             except TypeError as err:
                 msg = sys.stderr.getvalue()
                 msg_end = 'error: argument --linear: not allowed with argument --circ\n'
@@ -212,56 +216,56 @@ class TestParseArgs(IntegronTest):
                 sys.exit = real_exit
 
     def test_topology_file(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertIsNone(cfg.topology_file)
-        topo = 'foo'
-        cfg = parse_args(['--topology-file', topo, 'replicon'])
+        topo = self.find_data('topology.txt')  # whatever the value the file must exists
+        cfg = parse_args(['--topology-file', topo, self.replicon])
         self.assertEqual(cfg.topology_file, topo)
 
     def test_mute(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.mute)
-        cfg = parse_args(['--mute', 'replicon'])
+        cfg = parse_args(['--mute', self.replicon])
         self.assertTrue(cfg.mute)
 
     def test_verbose(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.verbose, 0)
-        cfg = parse_args(['--verbose', 'replicon'])
+        cfg = parse_args(['--verbose', self.replicon])
         self.assertEqual(cfg.verbose, 1)
-        cfg = parse_args(['-vv', 'replicon'])
+        cfg = parse_args(['-vv', self.replicon])
         self.assertEqual(cfg.verbose, 2)
 
     def test_quiet(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertEqual(cfg.quiet, 0)
-        cfg = parse_args(['--quiet', 'replicon'])
+        cfg = parse_args(['--quiet', self.replicon])
         self.assertEqual(cfg.quiet, 1)
-        cfg = parse_args(['-qq', 'replicon'])
+        cfg = parse_args(['-qq', self.replicon])
         self.assertEqual(cfg.quiet, 2)
 
     def test_pdf(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.pdf)
-        cfg = parse_args(['--pdf', 'replicon'])
+        cfg = parse_args(['--pdf', self.replicon])
         self.assertTrue(cfg.pdf)
 
     def test_gbk(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.gbk)
-        cfg = parse_args(['--gbk', 'replicon'])
+        cfg = parse_args(['--gbk', self.replicon])
         self.assertTrue(cfg.gbk)
 
     def test_keep_tmp(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.keep_tmp)
-        cfg = parse_args(['--keep-tmp', 'replicon'])
+        cfg = parse_args(['--keep-tmp', self.replicon])
         self.assertTrue(cfg.keep_tmp)
 
     def test_split_results(self):
-        cfg = parse_args(['replicon'])
+        cfg = parse_args([self.replicon])
         self.assertFalse(cfg.split_results)
-        cfg = parse_args(['--split-results', 'replicon'])
+        cfg = parse_args(['--split-results', self.replicon])
         self.assertTrue(cfg.split_results)
 
 
