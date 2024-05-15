0e70215b319ffbe083629cbf921deed86c414b9e
Bertrand Néron <bneron@pasteur.fr>
2024-05-15 15:46:54 +0200

    :arrow_up: bump IF version to 2.0.4.dev1

--- integron_finder/__init__.py
+++ integron_finder/__init__.py
@@ -32,7 +32,7 @@ import  subprocess
 from time import localtime, strftime
 
 
-__version__ = f'{strftime("%Y%m%d", localtime())}.dev'
+__version__ = '2.0.4dev1'
 
 
 def get_git_revision_short_hash():
