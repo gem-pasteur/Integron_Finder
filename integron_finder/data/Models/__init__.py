c76ebf54ae5e078afdcee1f0a1390ffb6ba42ef5
Bertrand Néron <bneron@pasteur.fr>
2025-05-07 10:58:32 +0200

    add __init__ in data packages to make setuptools happy

---
+++ integron_finder/data/Models/__init__.py
@@ -0,0 +1,25 @@
+####################################################################################
+# Integron_Finder - Integron Finder aims at detecting integrons in DNA sequences   #
+# by finding particular features of the integron:                                  #
+#   - the attC sites                                                               #
+#   - the integrase                                                                #
+#   - and when possible attI site and promoters.                                   #
+#                                                                                  #
+# Authors: Jean Cury, Bertrand Neron, Eduardo PC Rocha                             #
+# Copyright (c) 2015 - 2025  Institut Pasteur, Paris and CNRS.                     #
+# See the COPYRIGHT file for details                                               #
+#                                                                                  #
+# integron_finder is free software: you can redistribute it and/or modify          #
+# it under the terms of the GNU General Public License as published by             #
+# the Free Software Foundation, either version 3 of the License, or                #
+# (at your option) any later version.                                              #
+#                                                                                  #
+# integron_finder is distributed in the hope that it will be useful,               #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of                   #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the                    #
+# GNU General Public License for more details.                                     #
+#                                                                                  #
+# You should have received a copy of the GNU General Public License                #
+# along with this program (COPYING file).                                          #
+# If not, see <http://www.gnu.org/licenses/>.                                      #
+####################################################################################
